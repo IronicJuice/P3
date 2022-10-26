e37ef79c31a883ad2d344d964249538f42578388
IronicJuice <81492870+IronicJuice@users.noreply.github.com>
2022-10-26 15:18:43 +0200

    Added more input elements, and started code injection

---
+++ RefundForm/FormInfo.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RefundForm
+{
+    public class FormInfo
+    {
+        public void testFunc()
+        {
+            Console.WriteLine("My god! It works!");
+        }
+
+    }
+}
