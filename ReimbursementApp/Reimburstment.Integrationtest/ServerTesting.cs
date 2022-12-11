348b7610725063d73afefa709863f68fbd25484a
Paetur Magnussen <pmagnu19@student.aau.dk>
2022-12-11 23:04:49 +0100

    Started integration test, but have not made any test yet

---
+++ ReimbursementApp/Reimburstment.Integrationtest/ServerTesting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+
+
+namespace Reimburstment.Integrationtest
+{
+    public class ServerTesting
+    {
+        [Fact]
+        public async void randomtest()
+        {
+
+            //var wepfactory = new WebApplicationFactory<Program>();
+            //var httpclient = wepfactory.CreateDefaultClient();
+
+            //var respons = await httpclient.GetAsync("/");
+            //var result = await respons.Content.ReadAsStringAsync();
+
+            //Assert.NotNull(result);
+        }
+    }
+}
